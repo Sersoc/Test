68f200397aec69c06b6c5215098842779f2a3209
Sersoc <45021157+Sersoc@users.noreply.github.com>
2024-08-01 01:42:37 +0900

    test
    test

---
+++ TEst/TEst/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TEst
+//
+//  Created by 이재익 on 8/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
